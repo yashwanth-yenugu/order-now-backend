c95aabbc65ca459a699e15b889a55c7a469fb3d8
Yashwanth Reddy <yashwanth.reddy@abjayonconsulting.com>
2020-02-28 23:04:42 +0530

    format

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,33 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import * as AWS from "aws-sdk";
-
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class AdminService {
-    constructor() {
-        const awsConfig = {
-            "region": "us-east-1",
-            "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
-            "accessKeyId": "AKIAWBVAHP7QIQSNWZOI",
-            "secretAccessKey": "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"
-        };
-        AWS.config.update(awsConfig);
-    }
+  constructor() {
+    const awsConfig = {
+      region: 'us-east-1',
+      endpoint: 'http://dynamodb.us-east-1.amazonaws.com',
+      accessKeyId: 'AKIAWBVAHP7QIQSNWZOI',
+      secretAccessKey: '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',
+    };
+    AWS.config.update(awsConfig);
+  }
 
-  async  getRestaurants() {
-      return new Promise((resolve,reject)=>{    
-        const docClient = new AWS.DynamoDB.DocumentClient();
-        const params = {
-            TableName:"Restaurants",
-            Select: "ALL_ATTRIBUTES"
-        };
-        docClient.scan(params, (err, data) => {
-         console.log('err, data------------',err, data);
-         if(!err){
-         resolve(data.Items);
-         }
-         reject(err);
-        })
-    })
-    }
+  async getRestaurants() {
+    return new Promise((resolve, reject) => {
+      const docClient = new AWS.DynamoDB.DocumentClient();
+      const params = {
+        TableName: 'Restaurants',
+        Select: 'ALL_ATTRIBUTES',
+      };
+      docClient.scan(params, (err, data) => {
+        console.log('err, data------------', err, data);
+        if (!err) {
+          resolve(data.Items);
+        }
+        reject(err);
+      });
+    });
+  }
 }
